430579f8acb50c62a02ad5a6ac672e37b8fa70c8
Vamsi Krishna Nagaraju <vamsi8979@gmail.com>
2023-07-01 22:35:35 +0530

    added interpolation

---
+++ interpolation/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Codinmg Maxima';
+  title2 = 'Everyone Can Code';
+
+  listelement1 = 'Learn Coding from Top Developers.';
+  listelement2 = 'Learn Code Debugging with animation videos';
+  listelement3 = 'Learn in live hands on classrooms';
+  listelement4 = 'Structured, flexible curriculum';
+  listelement5 = 'Weekly Code Reviews and live mentoring';
+  listelement6 = 'Interview training and one on one sessions';
+
+  pelement = 'Welcome to Coding Maxima where you can learn coding from top developers and learn through animations'
+}
