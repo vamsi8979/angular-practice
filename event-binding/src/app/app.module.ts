cc0b273064045a2bbf09e62664f20e52d6047e89
Vamsi Krishna Nagaraju <vamsi8979@gmail.com>
2023-07-02 15:57:48 +0530

    added event binding project

---
+++ event-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StudentFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
