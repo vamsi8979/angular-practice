1020a60f587fad861a8b27a49098e0859ac2da67
Vamsi Krishna Nagaraju <vamsi8979@gmail.com>
2023-07-09 13:22:12 +0530

    added event binding

--- event-binding/src/app/student-form/student-form.component.ts
+++ event-binding/src/app/student-form/student-form.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-form.component.css']
 })
 export class StudentFormComponent {
+  name:string='John Doe';
+  age:string='30';
+  email:string='John@gmail.com';
 
+  setName(name:any){
+    this.name = name.target.value;
+  }
+  setAge(age:any){
+    this.age = age.target.value;
+  }
+  setEmail(email:any){
+    this.email = email.target.value;
+  }
+  onSubmit(){
+    alert(' Form Submitted : \n'+ ' Name : '+this.name+ '\n' + ' Age: ' + this.age +'\n' + ' Email : '+ this.email );
+  }
 }
